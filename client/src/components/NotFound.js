adf2e6c06e79a98f8dd01586faec2bdfa4ec7d19
Rachel Klimek <rachel@medable.com>
2020-04-01 21:35:03 -0400

    Update Language

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default () => (
   <div className="bounds">
     <h1>Not Found</h1>
-    <p>Sorry! We couldn't find the page you're looking for.</p>
+    <p>Sorry! Page Not Found.</p>
   </div>
 );
